280475d01b27636a90591091043984b0276676fd
Bruna Mazzaron <69203877+brunamazzaron@users.noreply.github.com>
2021-09-29 20:07:23 -0300

    Instanciando uma Classe

---
+++ src/main/kotlin/fundamentos/Carro.kt
@@ -0,0 +1,20 @@
+package fundamentos
+
+class Carro( var cor: String, var anoDeFabricacao: Int, val proprietario: Proprietario) {
+}
+
+data class Proprietario(var nome: String, var idade: Int){
+}
+
+fun main(){
+    var carro = Carro ("Branca", 1996, Proprietario("Bruna", 28))
+
+    println(carro.cor)
+    println(carro.anoDeFabricacao)
+    println(carro.proprietario)
+    println()
+    carro.cor = "Preto"
+    carro.anoDeFabricacao = 2021
+    println(carro.cor)
+    println(carro.anoDeFabricacao)
+}
