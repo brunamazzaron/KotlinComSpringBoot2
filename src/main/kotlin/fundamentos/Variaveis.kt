7ea52ae5e77d073a2d1ec52b79e27097e8715338
Bruna Mazzaron <69203877+brunamazzaron@users.noreply.github.com>
2021-10-05 07:57:19 -0300

    É um modificador que possibilita inicializar a variável depois de sua declaração, ou seja, inicializar tardiamente uma propriedade.

---
+++ src/main/kotlin/fundamentos/Variaveis.kt
@@ -0,0 +1,18 @@
+package fundamentos
+
+fun main(){
+    var nome = "Bruna"
+    val nomeVal = "Bruna"
+    nome  = "Vinicius"
+    val idade = 28
+    println(nome)
+    println(nomeVal)
+    println(idade)
+}
+
+//val nao pode ser reatribuída (Val cannot be reassigned)
+class Variaveis {
+    val teste: Int = 45
+    //as variaveis dentro da classe precisam ser iniciadas
+    // lateinit
+}
