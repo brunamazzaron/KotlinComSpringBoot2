958214472cedff3da754f13122531a0a158bc3f2
Bruna Mazzaron <69203877+brunamazzaron@users.noreply.github.com>
2021-10-05 17:31:57 -0300

    notas sobre tipos e Top 15 IntelliJ IDEA shortcuts

---
+++ src/main/kotlin/fundamentos/Notas.kt
@@ -0,0 +1,37 @@
+package fundamentos
+
+class Notas {
+}
+
+/* atalhos youtube.com/watch?v=QYO5_riePOQ&ab_channel=IntelliJIDEAbyJetBrains*/
+// alt + enter - sugestão de fixs ou F2
+// ctrl + k - para commitar
+// comandand + 1 ou alt + 1 -> navega pelo projeto
+//esc - faz algo q eu nao entendi
+// command + e  ou  ctrl + e ->acho que procura
+// command + b ou ctrl +b -> navega pelos arquivos q tem aquela funcao, classe, objeto
+// command b + ctrl+b -> navega nas classes
+//alt + f7
+
+//Types
+/*Numbers, byte, short, int, long  É um modificador que possibilita inicializar a
+variável depois de sua declaração, ou seja, inicializar tardiamente uma propriedade.
+
+
+val one = 1 // Int
+val threeBillion = 3000000000 // Long
+val oneLong = 1L // Long
+val oneByte: Byte = 1
+
+val pi = 3.14 // Double
+// val one: Double = 1 // Error: type mismatch
+val oneDouble = 1.0 // Double
+
+val e = 2.7182818284 // Double
+val eFloat = 2.7182818284f // Float, actual value is 2.7182817
+
+
+*/
+
+
+
