a1b98efcc233067ede710ac8c592611aa8da11cf
Bruna Mazzaron <69203877+brunamazzaron@users.noreply.github.com>
2021-11-01 12:58:40 -0300

    Posso usar duas funcoes diferentes fazendo um laço de repetição parecidos sem conflitar com o nome atribuido ao resultado.
    O uso de downTo para ordem decrescente

--- src/main/kotlin/fundamentos/Repeticoes.kt
+++ src/main/kotlin/fundamentos/Repeticoes.kt
@@ -2,10 +2,19 @@ package fundamentos
 
 fun main(){
     printar1a10()
+    println("--------------------")
+    printar10a1()
+
 }
 
 fun printar1a10(){
     for(numero in 1 .. 10){
+        println(numero)
+    }
+}
+
+fun printar10a1(){
+    for(numero in 10 downTo 1){
         println(numero)
     }
 }
